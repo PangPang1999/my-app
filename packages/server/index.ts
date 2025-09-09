1da7770582c6aed410efddad5e79c4e36e51b200
Yi Miao <ppp_melody@163.com>
2025-09-09 18:25:07 +0800

    Extract chat controller

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { chatController } from './controllers/chat.controller';
+import router from './routes';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use(router);
+
 const port = process.env.PORT || 3000;
-
-app.post('/api/chat', chatController.sendMessage);
-
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
