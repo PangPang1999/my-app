699b14b87b2240fb4d318f6d5f83fbdf9e61da25
Yi Miao <ppp_melody@163.com>
2025-09-09 17:16:03 +0800

    Extract chat service

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,14 +1,11 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import OpenAI from 'openai';
 import z from 'zod';
-import { conversationRepository } from './repositories/conversation.repository';
+import { chatService } from './services/chat.service';
 
 dotenv.config();
-const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
@@ -18,7 +15,7 @@ const schema = z.object({
     .string()
     .min(1, 'Prompt is required')
     .max(1000, 'Prompt is too long'),
-  conversationsId: z.string().uuid(),
+  conversationId: z.string().uuid(),
 });
 
 app.post('/api/chat', async (req: Request, res: Response) => {
@@ -29,17 +26,10 @@ app.post('/api/chat', async (req: Request, res: Response) => {
   }
 
   try {
-    const { prompt, conversationsId } = req.body;
-    const responses = await client.responses.create({
-      model: 'gpt-4o-mini',
-      input: prompt,
-      temperature: 0.2,
-      max_output_tokens: 100,
-      previous_response_id:
-        conversationRepository.getLastResponseId(conversationsId),
-    });
-    conversationRepository.setLastResponseId(conversationsId, responses.id);
-    res.json({ message: responses.output_text });
+    const { prompt, conversationId } = req.body;
+    const response = await chatService.sendMessage(prompt, conversationId);
+
+    res.json({ message: response.message });
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch response from OpenAI' });
   }
