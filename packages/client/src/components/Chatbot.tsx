d293ebd24f58ae09228160b56a2800a63796787c
Yi Miao <ppp_melody@163.com>
2025-09-10 11:11:52 +0800

    Handle form submission

--- packages/client/src/components/Chatbot.tsx
+++ packages/client/src/components/Chatbot.tsx
@@ -1,18 +1,46 @@
 import { FaArrowUp } from 'react-icons/fa';
 import { Button } from './ui/button';
+import type { KeyboardEvent } from 'react';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+  prompt: string;
+};
 
 const ChatBot = () => {
+  const { register, handleSubmit, reset, formState } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+    reset();
+  };
+
+  const onKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault(); // 阻止默认提交 → 改为手动调用 handleSubmit(onSubmit)()，这样可以走自己定义的表单校验和提交逻辑，而不是浏览器的默认逻辑。
+      handleSubmit(onSubmit)();
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      onKeyDown={onKeyDown}
+      className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+    >
       <textarea
+        {...register('prompt', {
+          required: true,
+          validate: (data) => data.trim().length > 0,
+        })}
         className="w-full border-0 focus:outline-0 resize-none"
         placeholder="Ask anything"
         maxLength={1000}
       />
-      <Button className="rounded-full w-9 h-9">
+      <Button disabled={!formState.isValid} className="rounded-full w-9 h-9">
         <FaArrowUp />
       </Button>
-    </div>
+    </form>
   );
 };
 
